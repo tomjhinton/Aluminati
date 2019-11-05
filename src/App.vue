64d6fb2316345255759b41142b16edc4ba46c323
Tom Hinton <tomjhinton@gmail.com>
2019-11-05 11:12:26 +0000

    git  setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <input type="number" v-model="limit" />
-    <Numbers/>
+    <input type="number" align='middle' v-model="limit" value=this.limit  />
+    <Numbers @clicked="pick"/>
   </div>
 </template>
 
@@ -16,9 +16,40 @@ export default {
     return {
       limit: 100
     }
+  },
+  // recieves new limit if triggered by click in child element
+  methods: {
+    pick(value) {
+      this.limit = value
+    }
   }
 }
 </script>
 
 <style scoped>
+
+#app{
+  background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%,     rgba(9,121,76,1.16150210084033612) 35%, rgba(0,212,255,1) 100%);
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+
+}
+
+input{
+  display: flex;
+  margin:auto;
+  margin-top:2em;
+  margin-bottom: 2em;
+  padding: 1em;
+  text-align: center;
+  border-radius: 50%;
+  font-size: 2em;
+  font-family: "Helvetica Neue";
+  border: 2px  solid white;
+  background-color: rgba(0,0,0,0);
+  color: white;
+
+}
 </style>
--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
+  <div  id="numbers">
+    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset" @click="pick">
       {{number}}
     </div>
   </div>
@@ -25,23 +25,35 @@ export default {
     n()
     {
       let numbers = [];
-      for(var i = 0; i < this.limit; i++)
+      for(var i = 0; i <= this.limit; i++) //  included the value in the input box as opposed to stopping at one below it
       {
         numbers = [...numbers, i];
       }
-      return numbers.sort(() => Math.random() - 0.5);
+      // replaced numbers.sort(() => Math.random() - 0.5) with a Fisher-Yates sort as it gives a better random output
+      for (let i = numbers.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  return numbers
     },
     hov(number)
     {
       const nums = document.querySelectorAll('.number');
 
+
       for(let i = 0; i < nums.length; i++)
       {
         const num = nums[i].textContent.trim();
-        if(number % num === 0)
+        // differentiated between the selected number and it's divisors
+        if(number % num === 0 && number >  num)
         {
           nums[i].classList.add('active')
-          console.log('divisor', num)
+          //console.log('divisor', num)
+        }
+        if(number % num === 0 && number ==  num)
+        {
+          nums[i].classList.add('chosen')
+          //console.log('divisor', num)
         }
       }
     },
@@ -49,20 +61,53 @@ export default {
     {
       const nums = document.querySelectorAll('.number');
       nums.forEach(num => num.classList.remove('active'))
+      nums.forEach(num => num.classList.remove('chosen'))
+    },
+    //lets you click on a number to make that the new number of randomly ordered numbers
+    pick(number){
+      this.limit = number.path[0].innerText
+      this.$emit('clicked', this.limit)
+
     }
   }
 }
 </script>
 
 <style scoped>
+
+  *{
+  font-family: "Helvetica Neue";
+  }
+
+  #numbers{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+
+  }
+
 	.number {
-		display: inline-block;
+    width: 2em;
+    height: 2em;
 		padding: 5px;
-		background-color: lightgrey;
-		margin: 5px;
+		background-color: rgba(0,0,0,0);
+		margin: 0.5em;
+    font-size: 2em;
+    color: white;
+    border: 2px  solid  white;
+    text-align: center;
+    line-height: 2em;
+
 	}
 
 	.active {
 		background-color: red;
+    transition: 2s;
 	}
+  .chosen {
+		background-color: blue;
+    transition: 2s;
+	}
+
 </style>
